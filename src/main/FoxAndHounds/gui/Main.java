18b427686604c6248f13dccd96fa272af03dbaa1
Abel Molnar <molnar.q@gmail.com>
2022-10-17 00:23:38 +0200

    Beadando

--- src/main/FoxAndHounds/gui/Main.java
+++ src/main/FoxAndHounds/gui/Main.java
@@ -1,8 +1,11 @@
 package main.FoxAndHounds.gui;
 
+import java.awt.FontFormatException;
+import java.io.IOException;
+import java.text.ParseException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
+import javax.swing.UnsupportedLookAndFeelException;
 
 public class Main {
 
